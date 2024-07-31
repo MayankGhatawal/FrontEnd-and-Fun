7ee78d0fe64f7f77f06c817cef795b6c7a4a2d2d
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-08-01 01:12:17 +0530

    main

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../image/mc.png'
-
+import tire from '../../image/menu.png'
 function Header() {
     return(
         <nav>
-            <img src={logo} alt="logo" />
+            <img className='voti' src={logo} alt="logo" />
             <ul>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
+            <a href="./head.html" target='_rel'><img className='imgage' src={tire} alt="menu"/></a>
             </ul>
         </nav>
     );
