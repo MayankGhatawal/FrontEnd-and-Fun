ce7c1746a1f32b1d821d4e0a64c01946af8d0131
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-08-06 15:47:28 +0530

    main

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <>
-      
+      <div className="page1">
+        <div className="page1-hidden">
+
+        </div>
+
+      </div>
+      <div className="page2">
+      <div className="page2-hidden">
+          
+          </div>
+
+      </div>
+      <div className="page3">
+      <div className="page3-hidden">
+          
+          </div>
+
+      </div>
     </>
   )
 }
