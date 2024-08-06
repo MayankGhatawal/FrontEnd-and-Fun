0ca6cfecced1f2d5f8f1cbda4efa7e6d7acc0937
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-08-06 16:12:37 +0530

    main

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -1,6 +1,14 @@
+import React from 'react'
 import './App.css'
+import { useGSAP } from '@gsap/react'
 
 function App() {
+
+  useGSAP(()=> {
+    gsap.from('.page1-hidden', { duration: 1, x: -100, ease: 'elastic.in' })
+    gsap.from('.page2-hidden', { duration: 1, x: 100, ease: 'elastic.in' })
+    gsap.from('.page3-hidden', { duration: 1, x: 200, ease: 'elastic.in' })
+  })
 
   return (
     <>
